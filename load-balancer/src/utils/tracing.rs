eb9743c8deacdfe9ade2e0e22a3ed323b9a270ca
zainen <zainensuzuki@gmail.com>
2024-12-07 21:56:18 -0800

    feature: add health checks spawn task, refactor rwlock unneeded nested

--- load-balancer/src/utils/tracing.rs
+++ load-balancer/src/utils/tracing.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use color_eyre::eyre::Result;
 use tokio::net::TcpStream;
 use tracing::{Level, Span};
@@ -28,24 +26,5 @@ pub fn make_span_with_request_id(_request: TcpStream) -> Span {
 
 pub fn on_request(_request: TcpStream, _span: &Span) {
     tracing::event!(Level::INFO, "[REQUEST START]");
-}
-
-pub fn on_response(response: TcpStream, latency: Duration, _span: &Span) {
-
-    // match status_code_class {
-    //     4..=6 => {
-    //         tracing::event!(
-    //             Level::ERROR,
-    //             "[REQUEST END]"
-    //         )
-    //     }
-    //     _ => {
-    //         tracing::event!(
-    //             Level::INFO,
-    //                 latency = ?latency,
-    //                 "[REQUEST END]"
-    //         )
-    //     }
-    // }
 }
 
